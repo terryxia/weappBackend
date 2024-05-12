c2642add189060664924ea8542d1ae55f5e699ae
terryxia <shanglinxia1129@163.com>
2024-05-13 00:00:56 +0800

    add redis

---
+++ redis/redisOptions.js
@@ -0,0 +1,8 @@
+const options = {
+    host: 'localhost',
+    port: 6379,
+    // password: '', // 本人未设置密码，所以隐藏，若你设了密码，则输入密码
+    detect_buffers: true // 传入buffer 返回也是buffer 否则会转换成String
+   }
+    
+   module.exports = options;
