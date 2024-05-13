308628ca87f3049607b45e22525506ab955ddd23
terryxia <shanglinxia1129@163.com>
2024-05-13 13:29:58 +0800

    phone number

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ var path = require('path')
 var session = require('express-session')
 var axios = require('axios')
 var config = require('./config')
-var WXBizDataCrypt = require('./WXBizDataCrypt')
+var WXBizDataCrypt = require('./WXBizDataCrypt');
+
+const redis = require('./redis/redisConfig');
+
+
+
 var app = express()
 
 const port = 9000
@@ -44,7 +49,7 @@ app
 
   .post('/oauth/login', (req, res) => {
     var params = req.body
-    var {code, type} = params
+    var {code, type, encryptedData, iv} = params
     if (type === 'wxapp') {
       axios.get('https://api.weixin.qq.com/sns/jscode2session', {
         params: {
@@ -54,31 +59,70 @@ app
           grant_type: 'authorization_code'
         }
       }).then(({data}) => {
-        console.log('data---', data);
-        var openId = data.openid
-        var user = users[openId]
-        if (!user) {
-          user = {
-            openId,
-            sessionKey: data.session_key
-          }
-          users[openId] = user
-          console.log('新用户', user)
-        } else {
-          console.log('老用户', user)
+        var openId = data.openid;
+        var sessionKey = data.session_key;
+
+        redis.setValue(openId, sessionKey);
+
+        var pc = new WXBizDataCrypt(config.appId, sessionKey);
+        var phoneInfo;
+        try {
+          phoneInfo = pc.decryptData(encryptedData, iv);
+          console.log('用户手机号数据:', phoneInfo);
+        } catch (err) {
+          throw new Error('session 失效建议重新登录')
         }
-        req.session.openId = user.openId
+
+
+        // console.log('data---', data);
+        // var openId = data.openid
+        // var user = users[openId]
+        // if (!user) {
+        //   user = {
+        //     openId,
+        //     sessionKey: data.session_key
+        //   }
+        //   users[openId] = user
+        //   console.log('新用户', user)
+        // } else {
+        //   console.log('老用户', user)
+        // }
+        // req.session.openId = user.openId
       }).then(() => {
         res.send({
             errno: 0,
             data: {
-              name: '111'
+              name: '111',
+              phoneInfo
             }
           })
       })
     } else {
       throw new Error('未知的授权类型')
     }
+  })
+
+  
+  .post('/user/bindphone', (req, res) => {
+    var user = req.user
+    if (user) {
+      var {encryptedData, iv, openId} = req.body
+      const session_key = redis.getValue();
+      var pc = new WXBizDataCrypt(config.appId, user.sessionKey);
+      var data;
+      try {
+        data = pc.decryptData(encryptedData, iv);
+        console.log('用户数据:', data);
+      } catch (err) {
+        throw new Error('session 失效建议重新登录')
+      }
+      // console.log('用户数据:', data);
+      Object.assign(user, data)
+      return res.send({
+        code: 0
+      })
+    }
+    throw new Error('用户未登录')
   })
 
   .get('/user/info', (req, res) => {
@@ -109,26 +153,6 @@ app
     throw new Error('用户未登录')
   })
 
-  .post('/user/bindphone', (req, res) => {
-    var user = req.user
-    if (user) {
-      var {encryptedData, iv} = req.body
-      var pc = new WXBizDataCrypt(config.appId, user.sessionKey);
-      var data;
-      try {
-        data = pc.decryptData(encryptedData, iv);
-        console.log('用户数据:', data);
-      } catch (err) {
-        throw new Error('session 失效建议重新登录')
-      }
-      // console.log('用户数据:', data);
-      Object.assign(user, data)
-      return res.send({
-        code: 0
-      })
-    }
-    throw new Error('用户未登录')
-  })
 
   .use(function (err, req, res, next) {
     console.log('err', err.message)
